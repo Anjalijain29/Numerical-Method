3c110265f55e5caa80965c0d5044202f107601b1
Anjalijain29 <60378188+Anjalijain29@users.noreply.github.com>
2020-02-28 22:31:29 +0530

    complete newton raphson method

---
+++ Newton Raphson Method.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float x)
+{
+  float y;
+  y = (x*x*x) - 20;
+  return y;
+}
+
+float df(float x)
+{
+  float y;
+  y = (3*x*x);
+  return y;
+}
+
+void main()
+{
+  float e, x0, x1, f0, f1, df0;
+  int count = 0;
+  printf("Enter accuracy: \n");
+  scanf("%f", &e);
+
+  printf("Enter the value of x0: \n");
+  scanf("%f", &x0);
+
+  printf("Iterations| root\t | f(x1)\n");
+  do
+  {
+    f0 = f(x0);
+    df0 = df(x0);
+    x1 = x0 - (f0/df0);
+    f1 = f(x1);
+    x0 = x1;
+    count++;
+    printf("%d\t   %2.4f\t  %2.4f\n", count, x1, f1);
+  }while(fabs(f1) > e);
+
+  printf("The value of root is %2.4f\n", x1);
+}
